663d7bab550e9f95b67339e921bd1b27a1bf0e8a
JosTheDude <jos@joscodes.xyz>
2025-09-29 10:54:55 -0400

    fix: simple voice chat version & version number bumps

--- Scripts/exports.sh
+++ Scripts/exports.sh
@@ -5,39 +5,48 @@
 # Usage: Run this script inside the Packwiz folder to export all versions in the folder
 
 EXPORTS_DIR="$(pwd)/exports"
-VERSION="1.0.1"
+VERSION="1.1.0"
 
 # In case directory does not exist
-mkdir -p "$EXPORTS_DIR" || { echo "Failed to create $EXPORTS_DIR"; exit 1; }
+mkdir -p "$EXPORTS_DIR" || {
+  echo "Failed to create $EXPORTS_DIR"
+  exit 1
+}
 
 # Directory Iteration
-for dir in */ ; do
-    if [ -d "$dir" ] && [ "${dir%/}" != "exports" ]; then
-        # Remove the trailing slash from the directory name for clean export names
-        dir_name="${dir%/}"
-	version="${VERSION}"
-        
-        echo "Processing directory: $dir_name"
+for dir in */; do
+  if [ -d "$dir" ] && [ "${dir%/}" != "exports" ]; then
+    # Remove the trailing slash from the directory name for clean export names
+    dir_name="${dir%/}"
+    version="${VERSION}"
 
-	mkdir -p "$EXPORTS_DIR/$dir_name" || { echo "Failed to create $EXPORTS_DIR/$dir_name"; exit 1; }
+    echo "Processing directory: $dir_name"
 
-        cd "$dir_name" || { echo "Failed to enter directory $dir_name"; continue; }
+    mkdir -p "$EXPORTS_DIR/$dir_name" || {
+      echo "Failed to create $EXPORTS_DIR/$dir_name"
+      exit 1
+    }
 
-        # CurseForge Export
-	# Disabled as No Curseforge Version Present
-        #packwiz curseforge export -o "$EXPORTS_DIR/${dir_name}/Soul-Realms-Modpack-${dir_name}-${version}.zip"
-        
-        # Modrinth Export
-        packwiz modrinth export -o "$EXPORTS_DIR/${dir_name}/Soul-Realms-Modpack-MR-${dir_name}-${version}.mrpack"
+    cd "$dir_name" || {
+      echo "Failed to enter directory $dir_name"
+      continue
+    }
 
-	packwiz curseforge export -o "$EXPORTS_DIR/${dir_name}/Soul-Realms-Modpack-CF-${dir_name}-${version}.zip" 
+    # CurseForge Export
+    # Disabled as No Curseforge Version Present
+    #packwiz curseforge export -o "$EXPORTS_DIR/${dir_name}/Soul-Realms-Modpack-${dir_name}-${version}.zip"
 
-        cd ..
+    # Modrinth Export
+    packwiz modrinth export -o "$EXPORTS_DIR/${dir_name}/Soul-Realms-Modpack-MR-${dir_name}-${version}.mrpack"
 
-        echo "Exported $dir_name successfully."
-    else
-        echo "Skipping $dir as it is not a directory or is the exports directory."
-    fi
+    packwiz curseforge export -o "$EXPORTS_DIR/${dir_name}/Soul-Realms-Modpack-CF-${dir_name}-${version}.zip"
+
+    cd ..
+
+    echo "Exported $dir_name successfully."
+  else
+    echo "Skipping $dir as it is not a directory or is the exports directory."
+  fi
 done
 
 echo "All exports completed successfully."
